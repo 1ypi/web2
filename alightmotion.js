892f4fa4d0226f3ea4774e776b68b3ea20acdae8
1ypi <137641825+1ypi@users.noreply.github.com>
2024-03-17 20:11:21 +0100

    Create alightmotion.js

---
+++ alightmotion.js
@@ -0,0 +1,74 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+var obj = JSON.parse($response.body);
+
+obj= {
+  "result" : {
+    "result" : "success",
+    "msTime" : 1670593113048,
+    "accountCreatedMillis" : null,
+    "licenses" : [
+      {
+        "productId" : "alightcreative.motion.1y_1y_t10",
+        "label" : null,
+        "benefits" : [
+          "RemoveWatermark",
+          "MemberEffects",
+          "ProjectPackageSharing",
+          "FutureMemberFeatures",
+          "AdvancedEasing",
+          "CameraObjects",
+          "LayerParenting"
+        ],
+        "period" : "1y",
+        "valid" : true,
+        "expires" : 1699389411000,
+        "details" : null,
+        "type" : "subscription",
+        "autoRenewing" : true,
+        "orderNumber" : "320001035735292",
+        "linkStatus" : "linked-nologin",
+        "store" : "apple_app_store"
+      }
+    ],
+    "warnings" : [
+
+    ]
+  }
+}
+
+
+
+$done({body: JSON.stringify(obj)});
+
+}
+/*
+     FILE ARCHIVED ON 19:04:32 May 30, 2023 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 19:10:14 Mar 17, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots: 0.168
+  exclusion.robots.policy: 0.148
+  cdx.remote: 0.21
+  esindex: 0.018
+  LoadShardBlock: 366.862 (6)
+  PetaboxLoader3.datanode: 290.009 (8)
+  load_resource: 264.141
+  PetaboxLoader3.resolve: 136.038
+  loaddict: 57.432
+*/
